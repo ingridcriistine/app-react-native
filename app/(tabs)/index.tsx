7cf2c455efcb54aa49e04743ffdc88039e4af01f
ingridcriistine <ingridrochacristine@gmail.com>
2024-10-16 17:11:37 -0300

    testes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Image, StyleSheet, Text } from 'react-native';
+import foto from "@/assets/images/foto.jpg";
 
 export default function HomeScreen() {
   return (
     <>
       <Text>Turma mara</Text>
+      <Image style={styles.img} source={foto} alt='imagem'/>
     </>
   );
 }
@@ -12,17 +14,21 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 8
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
+    marginBottom: 8
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: 'absolute'
   },
+  img: {
+    width: 400,
+    height: 600
+  }
 });
